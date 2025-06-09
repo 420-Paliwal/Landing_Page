ecd032d8b646f398b5b221af674e6192a3e35d18
420-Paliwal <harshpaliwal0420@gmail.com>
2025-06-09 21:20:13 +0530

    navbar font pixel check

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,10 +16,10 @@ const Navbar = () => {
             <li><a href="#home" className='hover:text-red-400'>More Option</a></li>
             <li><a href="#contact" className='hover:text-red-400'>Contact</a></li>
             </ul>
-            <button className='btn py-2 rounded-2xl px-8 border-2 border-red-500 text-red-500 cursor-pointer'>
+            <button className='btn py-2 font-bold rounded-2xl px-8 border-2 border-red-500 text-red-500 cursor-pointer'>
                 Log in
             </button>
-            <button className='btn py-2 px-8 rounded-2xl border-2 border-red-500 text-white bg-red-500 cursor-pointer'>
+            <button className='btn py-2 px-8 font-bold rounded-2xl border-2 border-red-500 text-white bg-red-500 cursor-pointer'>
                 Sign Up
             </button>
             </div>
